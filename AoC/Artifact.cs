779181a5c42799f02fab4f0cccedc97f5d7b7bcf
CuteDoge01 <88965540+CuteDoge01@users.noreply.github.com>
2022-06-05 00:46:41 +0300

    Added support for Aetherium, bugfixes

--- AoC/Artifact.cs
+++ AoC/Artifact.cs
@@ -17,6 +17,7 @@ namespace AOCMod
         private static System.Random random = new System.Random();
         private static int chosenID = -1;
         private static int aspectID = 0;
+        private static SceneType lastSceneType = RoR2.SceneType.Invalid;
         private static List<string> aspectNames = new List<string> { "ZetAspectBlue", "ZetAspectEarth", "ZetAspectHaunted", "ZetAspectLunar", "ZetAspectPoison", "ZetAspectRed", "ZetAspectVoid", "ZetAspectWhite" };
 
         public static void InitializeArtifact()
@@ -28,6 +29,7 @@ namespace AOCMod
         public static void Hooks()
         {
             RoR2.Stage.onStageStartGlobal += AOCProvideAspect;
+            //RoR2.Stage.onServerStageComplete += AOCTakeAwayAspectFallback;
             TeleporterInteraction.onTeleporterChargedGlobal += AOCTakeAwayAspect;
         }
 
@@ -52,6 +54,10 @@ namespace AOCMod
             {
                 aspectNames.Add("ZetAspectGold");
             }
+            if (PluginLoaded("com.KomradeSpectre.Aetherium"))
+            {
+                aspectNames.Add("ZetAspectSanguine");
+            }
             if (PluginLoaded("com.groovesalad.GrooveSaladSpikestripContent"))
             {
                 aspectNames.Add("ZetAspectPlated");
@@ -64,19 +70,24 @@ namespace AOCMod
             if (RunArtifactManager.instance.IsArtifactEnabled(MyArtifactDef))
             {
                 //Chat.SendBroadcastChat(new SimpleChatMessage { baseToken = "<color=#e5eefc>{0}</color>", paramTokens = new[] { $"111 chosenID: {chosenID}, AspectID: {aspectID}, NAME: {GetZetAspectID(aspectID)}, MAX PlayerID: {PlayerCharacterMasterController.instances.Count - 1}" } });
-
                 chosenID = random.Next(PlayerCharacterMasterController.instances.Count);
                 aspectID = random.Next(aspectNames.Count);
-
-                Chat.SendBroadcastChat(new SimpleChatMessage { baseToken = "<color=#e5eefc>{0}</color>", paramTokens = new[] { $"The artifact has chosen {PlayerCharacterMasterController.instances[chosenID].GetDisplayName()}" } });
-
-                //TPDespair.ZetAspects.Language.tokens.ContainsKey(ItemCatalog.GetItemDef(ItemCatalog.FindItemIndex(aspectNames[aspectID])).nameToken);
                 try
                 {
                     //Chat.AddMessage($"Current max aspect ID count is: {aspectNames.Count}");
                     CharacterMaster.readOnlyInstancesList[chosenID].inventory.GiveItemString(aspectNames[aspectID]);
                 }
                 catch { Chat.SendBroadcastChat(new SimpleChatMessage { baseToken = "<color=#e5eefc>{0}</color>", paramTokens = new[] { $"Could not add the item to the inventory of the player ID { chosenID }, perhaps CharacterMaster.readOnlyInstancesList has not been created or the ID is wrong" } }); }
+
+                Chat.SendBroadcastChat(new SimpleChatMessage { baseToken = "<color=#e5eefc>{0}</color>", paramTokens = new[] { $"The artifact has chosen {PlayerCharacterMasterController.instances[chosenID].GetDisplayName()}" } });
+                
+                if (lastSceneType == SceneType.Intermission)
+                {
+                    CharacterMaster.readOnlyInstancesList[chosenID].inventory.RemoveItem(ItemCatalog.FindItemIndex(aspectNames[aspectID]));
+                }
+
+                lastSceneType = obj.sceneDef.sceneType;
+                //TPDespair.ZetAspects.Language.tokens.ContainsKey(ItemCatalog.GetItemDef(ItemCatalog.FindItemIndex(aspectNames[aspectID])).nameToken);
                 //CharacterMaster.readOnlyInstancesList[chosenID].inventory.GiveItemString(GetZetAspectID(0));
                 //     PlayerCharacterMasterController.instances[chosenID].master.GetBody().inventory.GiveItemString(GetZetAspectID(aspectID));
             }
